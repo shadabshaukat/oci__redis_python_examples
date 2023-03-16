4b041a7e35ab849bf522598eb20bfbc5f35d484c
Shadab Mohammad <39692236+shadabshaukat@users.noreply.github.com>
2023-03-16 15:25:31 +1100

    Create oci_redis_load.py

---
+++ oci_redis_load.py
@@ -0,0 +1,38 @@
+import redis
+import logging
+import random
+import string
+
+# Enable logging
+logging.basicConfig(level=logging.DEBUG)
+
+# Connect to Redis
+redis_client = redis.StrictRedis(
+    host='amaaaaaap77apcqandw5nv2266miqop6d3adkg3zbcwdpykfnqf24ge66t3a-p.redis.ap-mumbai-1.oci.oraclecloud.com',
+    charset='utf-8',
+    ssl=True,
+    decode_responses=True,
+    port=6379
+)
+redis_client_replica = redis.StrictRedis(host='amaaaaaap77apcqandw5nv2266miqop6d3adkg3zbcwdpykfnqf24ge66t3a-r.redis.ap-mumbai-1.oci.oraclecloud.com', charset='utf-8', ssl=True, decode_responses=True, port=6379)
+
+def random_string(length):
+    return ''.join(random.choice(string.ascii_letters) for _ in range(length))
+
+# Generate and write 100 random records
+logging.info("Generating and writing 100 random records to Redis")
+keys = []
+for i in range(100):
+    key = f"key_{i}"
+    value = random_string(10)
+    keys.append(key)
+    redis_client.set(key, value)
+
+# Read the values for the keys
+logging.info("Reading 100 records from Redis")
+for key in keys:
+    value = redis_client_replica.get(key)
+    if value is not None:
+        print(f"Value for {key}: {value}")
+    else:
+        print(f"Key {key} not found")
